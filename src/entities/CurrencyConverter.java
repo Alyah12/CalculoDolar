2dc4e8856fe1bfe3cd15ce2efe9a349c8ac318b1
Heberth Santos <86444094+Alyah12@users.noreply.github.com>
2024-01-08 16:08:31 -0300

    Correção do erro.

--- src/entities/CurrencyConverter.java
+++ src/entities/CurrencyConverter.java
@@ -4,10 +4,12 @@ public class CurrencyConverter {
 
     public static double dolar;
     public static double dolarPrice;
+    public static double taxa = 6.0 / 100;
 
     public static double conversão (){
-        dolar += dolar * dolarPrice + 06 / 100;
-        return dolar;
+        dolar = dolar * dolarPrice;
+        double calculo = (dolar * 6) / 100;
+        var resultado = calculo + dolar;
+        return  resultado;
     }
-
 }
